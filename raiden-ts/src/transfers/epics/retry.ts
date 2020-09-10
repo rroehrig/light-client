ba5d9570c99a6ff0628a06bb699ec2115e264528
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2020-09-10 11:56:18 -0300

    sdk: unify retry messages epics

--- raiden-ts/src/transfers/epics/retry.ts
+++ raiden-ts/src/transfers/epics/retry.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { EMPTY, Observable } from 'rxjs';
-import { filter, mergeMap, withLatestFrom, switchMap, first } from 'rxjs/operators';
+import { combineLatest, EMPTY, Observable } from 'rxjs';
+import { filter, mergeMap, withLatestFrom } from 'rxjs/operators';
 
 import { RaidenAction } from '../../actions';
 import { messageSend } from '../../messages/actions';
 import { RaidenState } from '../../state';
 import { RaidenEpicDeps } from '../../types';
 import { isActionOf } from '../../utils/actions';
+import { Address } from '../../utils/types';
 import { get$ } from '../../db/utils';
 import {
   transferExpire,
@@ -20,227 +21,19 @@ import { transferKey } from '../utils';
 import { retrySendUntil$, exponentialBackoff } from './utils';
 
 /**
- * Handles a transferSigned action and retry messageSend.request until transfer is gone (completed
- * with success or error) OR Processed message for LockedTransfer received.
- * transferSigned for pending LockedTransfer's may be re-emitted on startup for pending transfer,
- * to start retrying sending the message again until stop condition is met.
- *
- * @param action$ - Observable of transferSigned actions
- * @param action - The {@link transferSigned} action
- * @param deps - Epics dependencies subset
- * @param deps.config$ - Observable of latest RaidenConfig
- * @param deps.db - Database instance
- * @returns - Observable of {@link messageSend.request} actions
- */
-const signedRetryMessage$ = (
-  action$: Observable<RaidenAction>,
-  action: transferSigned,
-  { db, config$ }: Pick<RaidenEpicDeps, 'db' | 'config$'>,
-): Observable<messageSend.request> => {
-  if (action.meta.direction !== Direction.SENT) return EMPTY;
-  return config$.pipe(
-    first(),
-    switchMap(({ pollingInterval, httpTimeout }) => {
-      const signed = action.payload.message;
-      const send = messageSend.request(
-        { message: signed },
-        { address: signed.recipient, msgId: signed.message_identifier.toString() },
-      );
-      const notifier = get$(db.transfers, transferKey(action.meta)).pipe(
-        filter(
-          (doc) =>
-            !!(
-              doc.transferProcessed ||
-              doc.unlockProcessed ||
-              doc.expiredProcessed ||
-              doc.channelClosed
-            ),
-        ),
-      );
-      // emit request once immediatelly, then wait until success, then retry every 30s
-      return retrySendUntil$(
-        send,
-        action$,
-        notifier,
-        exponentialBackoff(pollingInterval, httpTimeout * 2),
-      );
-    }),
-  );
-};
-
-/**
- * Handles a transferUnlock.success action and retry messageSend until confirmed.
- * transferUnlock.success for pending Unlock's may be re-emitted on startup for pending transfer, to
- * start retrying sending the message again until stop condition is met.
- *
- * @param action$ - Observable of transferUnlock.success actions
- * @param action - the transferUnlock.success action
- * @param deps - Epics dependencies subset
- * @param deps.config$ - Observable of latest RaidenConfig
- * @param deps.db - Database instance
- * @returns Observable of {@link messageSend.request} actions
- */
-const unlockedRetryMessage$ = (
-  action$: Observable<RaidenAction>,
-  action: transferUnlock.success,
-  { db, config$ }: Pick<RaidenEpicDeps, 'db' | 'config$'>,
-): Observable<messageSend.request> => {
-  if (action.meta.direction !== Direction.SENT) return EMPTY;
-  const doc$ = get$(db.transfers, transferKey(action.meta));
-  return doc$.pipe(
-    first(),
-    withLatestFrom(config$),
-    switchMap(([doc, { pollingInterval, httpTimeout }]) => {
-      const unlock = action.payload.message;
-      const send = messageSend.request(
-        { message: unlock },
-        { address: doc.partner, msgId: unlock.message_identifier.toString() },
-      );
-      const notifier = doc$.pipe(filter((doc) => !!(doc.unlockProcessed || doc.channelClosed)));
-      // emit request once immediatelly, then wait until respective success,
-      // then repeats until confirmed
-      return retrySendUntil$(
-        send,
-        action$,
-        notifier,
-        exponentialBackoff(pollingInterval, httpTimeout * 2),
-      );
-    }),
-  );
-};
-
-/**
- * Handles a transferExpire.success action and retry messageSend.request until transfer is gone (completed
- * with success or error).
- * transferExpire.success for pending LockExpired's may be re-emitted on startup for pending transfer, to
- * start retrying sending the message again until stop condition is met.
- *
- * @param action$ - Observable of transferUnlock.success actions
- * @param action - transferExpire.success action
- * @param deps - Epics dependencies subset
- * @param deps.config$ - Observable of latest RaidenConfig
- * @param deps.db - Database instance
- * @returns Observable of {@link messageSend.request} actions
- */
-const expiredRetryMessages$ = (
-  action$: Observable<RaidenAction>,
-  action: transferExpire.success,
-  { db, config$ }: Pick<RaidenEpicDeps, 'db' | 'config$'>,
-): Observable<messageSend.request> => {
-  if (action.meta.direction !== Direction.SENT) return EMPTY;
-  const doc$ = get$(db.transfers, transferKey(action.meta));
-  return doc$.pipe(
-    first(),
-    withLatestFrom(config$),
-    switchMap(([doc, { pollingInterval, httpTimeout }]) => {
-      const expired = action.payload.message;
-      const send = messageSend.request(
-        { message: expired },
-        {
-          address: doc.partner,
-          msgId: expired.message_identifier.toString(),
-        },
-      );
-      const notifier = doc$.pipe(filter((doc) => !!(doc.expiredProcessed || doc.channelClosed)));
-      // emit request once immediatelly, then wait until respective success,
-      // then retries until confirmed
-      return retrySendUntil$(
-        send,
-        action$,
-        notifier,
-        exponentialBackoff(pollingInterval, httpTimeout * 2),
-      );
-    }),
-  );
-};
-
-const secretRequestRetryMessage$ = (
-  action$: Observable<RaidenAction>,
-  action: transferSecretRequest,
-  { db, config$ }: Pick<RaidenEpicDeps, 'db' | 'config$'>,
-): Observable<messageSend.request> => {
-  if (action.meta.direction !== Direction.RECEIVED) return EMPTY;
-  const doc$ = get$(db.transfers, transferKey(action.meta));
-  return doc$.pipe(
-    first(),
-    withLatestFrom(config$),
-    switchMap(([doc, { pollingInterval, httpTimeout }]) => {
-      const request = action.payload.message;
-      const send = messageSend.request(
-        { message: request },
-        {
-          address: doc.transfer.initiator,
-          msgId: request.message_identifier.toString(),
-        },
-      );
-      // stop retrying when we've signed secretReveal, lock expired or channel closed;
-      // we could stop as soon as we know received.secret, but we use it to retry SecretReveal
-      // signature, if it failed for any reason
-      const notifier = doc$.pipe(
-        filter((doc) => !!(doc.secretReveal || doc.expired || doc.channelClosed)),
-      );
-      // emit request once immediatelly, then wait until respective success,
-      // then retries until confirmed
-      return retrySendUntil$(
-        send,
-        action$,
-        notifier,
-        exponentialBackoff(pollingInterval, httpTimeout * 2),
-      );
-    }),
-  );
-};
-
-const secretRevealRetryMessage$ = (
-  action$: Observable<RaidenAction>,
-  action: transferSecretReveal,
-  { db, config$ }: Pick<RaidenEpicDeps, 'db' | 'config$'>,
-): Observable<messageSend.request> => {
-  if (action.meta.direction !== Direction.RECEIVED) return EMPTY;
-  const doc$ = get$(db.transfers, transferKey(action.meta));
-  return doc$.pipe(
-    first(),
-    withLatestFrom(config$),
-    switchMap(([doc, { pollingInterval, httpTimeout }]) => {
-      const reveal = action.payload.message;
-      const send = messageSend.request(
-        { message: reveal },
-        {
-          address: doc.partner,
-          msgId: reveal.message_identifier.toString(),
-        },
-      );
-      // stop retrying when we were unlocked, secret registered or channel closed
-      // we don't test for expired, as we know the secret and must not accept LockExpired
-      const notifier = doc$.pipe(
-        filter((doc) => !!(doc.unlock || doc.secretRegistered || doc.channelClosed)),
-      );
-      // emit request once immediatelly, then wait until respective success,
-      // then retries until confirmed
-      return retrySendUntil$(
-        send,
-        action$,
-        notifier,
-        exponentialBackoff(pollingInterval, httpTimeout * 2),
-      );
-    }),
-  );
-};
-
-/**
- * Retry sending balance proof messages until their respective Processed
+ * Retry sending protocol messages until stop conditions are met.
  *
  * @param action$ - Observable of transferExpire.success actions
  * @param state$ - Observable of RaidenStates
  * @param deps - Epics dependencies
- * @param deps.latest$ - Latest observable
+ * @param deps.db - Database instance
  * @param deps.config$ - Config observable
  * @returns Observable of messageSend.request actions
  */
 export const transferRetryMessageEpic = (
   action$: Observable<RaidenAction>,
-  {}: Observable<RaidenState>,
-  deps: RaidenEpicDeps,
+  state$: Observable<RaidenState>,
+  { db, config$ }: RaidenEpicDeps,
 ): Observable<messageSend.request> => {
   return action$.pipe(
     filter(
@@ -252,16 +45,76 @@ export const transferRetryMessageEpic = (
         transferSecretReveal,
       ]),
     ),
-    mergeMap((action) =>
-      transferSigned.is(action)
-        ? signedRetryMessage$(action$, action, deps)
-        : transferUnlock.success.is(action)
-        ? unlockedRetryMessage$(action$, action, deps)
-        : transferExpire.success.is(action)
-        ? expiredRetryMessages$(action$, action, deps)
-        : transferSecretRequest.is(action)
-        ? secretRequestRetryMessage$(action$, action, deps)
-        : secretRevealRetryMessage$(action$, action, deps),
-    ),
+    withLatestFrom(config$),
+    mergeMap(([action, { pollingInterval, httpTimeout, revealTimeout }]) => {
+      const doc = db.transfers.by('_id', transferKey(action.meta));
+      const doc$ = get$(db.transfers, transferKey(action.meta));
+
+      let to: Address | undefined;
+      let stop$: Observable<any> | undefined;
+      switch (action.type) {
+        case transferSigned.type:
+          if (action.meta.direction === Direction.SENT) {
+            to = action.payload.partner;
+            stop$ = doc$.pipe(
+              filter(
+                (doc) =>
+                  !!(
+                    doc.transferProcessed ||
+                    doc.unlockProcessed || // unlock|expired shouldn't happen before transferProcessed
+                    doc.expiredProcessed ||
+                    doc.channelClosed
+                  ),
+              ),
+            );
+          }
+          break;
+        case transferUnlock.success.type:
+          if (action.meta.direction === Direction.SENT) {
+            to = action.payload.partner;
+            stop$ = doc$.pipe(filter((doc) => !!(doc.unlockProcessed || doc.channelClosed)));
+          }
+          break;
+        case transferExpire.success.type:
+          if (action.meta.direction === Direction.SENT) {
+            to = action.payload.partner;
+            stop$ = doc$.pipe(filter((doc) => !!(doc.expiredProcessed || doc.channelClosed)));
+          }
+          break;
+        case transferSecretRequest.type:
+          if (action.meta.direction === Direction.RECEIVED && doc) {
+            to = doc.transfer.initiator;
+            stop$ = combineLatest([state$, doc$]).pipe(
+              filter(
+                ([{ blockNumber }, doc]) =>
+                  /* doc.secret would be enough, but let's test secretReveal to possibly retry
+                   * failed RevealSecret sign prompts */
+                  !!(doc.secretReveal || doc.channelClosed) ||
+                  // or when we get inside the danger zone
+                  blockNumber > doc.expiration - revealTimeout,
+              ),
+            );
+          }
+          break;
+        case transferSecretReveal.type:
+          if (action.meta.direction === Direction.RECEIVED && doc) {
+            to = doc.partner;
+            stop$ = doc$.pipe(filter((doc) => !!(doc.unlock || doc.channelClosed)));
+          }
+          break;
+      }
+
+      if (!to || !stop$) return EMPTY;
+
+      return retrySendUntil$(
+        messageSend.request(
+          { message: action.payload.message },
+          { address: to, msgId: action.payload.message.message_identifier.toString() },
+        ),
+        action$,
+        stop$,
+        exponentialBackoff(pollingInterval, httpTimeout * 2),
+      );
+    }),
   );
 };
